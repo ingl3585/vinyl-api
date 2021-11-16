7d58738c9216f4caabf304c2f3713b22925532fd
Tony Ingle <tonyingle@Tonys-MacBook-Air.local>
2021-11-16 16:02:54 -0700

    added deleted and updated vinyl

--- controllers/vinyls.js
+++ controllers/vinyls.js
@@ -2,17 +2,39 @@ const express = require('express');
 const router = express.Router();
 const Vinyl = require('./../Vinyl');
 
-// CREATE
-router.post('/', (req, res) => {
-	console.log(req.body);
-	Vinyl.create({ albumName: 'test album', artistName: 'test artist' }).then(
-		(vinyl) => res.json({ status: 200, vinyl: vinyl })
+// INDEX
+// display all vinyls and return 200 ok
+router.get('/', (req, res) => {
+	Vinyl.find().then((vinyls) =>
+		res.status(200).json({ status: 200, vinyls: vinyls })
 	);
 });
 
-// INDEX
-router.get('/', (req, res) => {
-	Vinyl.find().then((vinyls) => res.json({ status: 200, vinyls: vinyls }));
+// CREATE
+// create vinyl and return 201 created
+router.post('/', (req, res) => {
+	Vinyl.create(req.body).then((vinyl) =>
+		res.status(201).json({ status: 201, vinyl: vinyl })
+	);
+});
+
+// DELETE /:id
+// delete vinyl and return 204 no content
+router.delete('/:id', (req, res) => {
+	Vinyl.deleteOne({ _id: req.params.id }).then(() => {
+		res.status(204).json();
+	});
+});
+
+// PUT /:id
+// update vinyl and return 200 ok
+// { new: true } shows updated vinyl
+router.put('/:id', (req, res) => {
+	Vinyl.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }).then(
+		(vinyl) => {
+			res.status(200).json(vinyl);
+		}
+	);
 });
 
 module.exports = router;
